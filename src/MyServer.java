5d94c4e0680c917700b81a426d399eaf00a3f3ae
SamuelsEdwin <samuelsedwin.js@gmail.com>
2018-10-31 07:08:21 +0200

    Server Addition

--- src/MyServer.java
+++ src/MyServer.java
@@ -3,9 +3,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
-/**
- * Written by Martin Ombura Jr. <@martinomburajr>
- */
+
 public class MyServer {
     public static void main(String[] args) {
         connectToServer();
